6835a90168ee8f4d870ce8aec1173520472bc885
yusufneeson <yusufneeson@gmail.com>
2022-08-30 15:16:16 +0700

    Index Page: `getStaticProps` countries

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
+import Countries from "../components/Countries/Countries";
 import Filter from "../components/Filter/Filter";
 import HomeLayout from "../layout/HomeLayout";
 
@@ -5,6 +6,19 @@ export default function Index({ countries }) {
 	return (
 		<HomeLayout>
 			<Filter />
+			<Countries countries={countries} />
 		</HomeLayout>
 	);
+}
+
+export async function getStaticProps() {
+	const res = await fetch(
+		"https://restcountries.com/v2/all?fields=flag,name,region,population,capital,alpha3Code"
+	);
+	const countries = await res.json();
+	return {
+		props: {
+			countries,
+		},
+	};
 }
