208be26b486c1214e50aff2591070d50d99bb09a
yusufneeson <yusufneeson@gmail.com>
2022-08-30 11:55:57 +0700

    HomeLayout: Navbar and apply next-themes

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,11 +1,21 @@
+import { useTheme } from "next-themes";
 import Head from "next/head";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import Moon from "../components/icons/Moon";
+import Sun from "../components/icons/SUn";
+import style from "./homelayout.module.css";
 
 function HomeLayout(props) {
 	const { title = "", children } = props;
+	const { theme, setTheme } = useTheme();
+	const [mounted, setMounted] = useState(false);
+	useEffect(() => setMounted(true), []);
+	const isDark = theme === "dark";
 	return (
 		<>
 			<Head>
-				<title>{title ? `${title} |` : ""} Next.js Countries</title>
+				<title>Next.js Countries</title>
 				<meta
 					name="description"
 					content="List all countries using REST API with Next.js by Yusuf Neeson"
@@ -13,7 +23,36 @@ function HomeLayout(props) {
 				<meta name="keywords" content="next.js, countries, rest, api" />
 				<meta name="author" content="Yusuf Neeson" />
 			</Head>
-			{children}
+			<header className={style.header}>
+				<nav className={style.nav}>
+					<h1 className={style.title}>
+						<Link href="/">
+							<a>Where in the world ?</a>
+						</Link>
+					</h1>
+					<div
+						onClick={() => setTheme(isDark ? "light" : "dark")}
+						className={style.switcher}
+					>
+						{mounted && (
+							<>
+								{isDark ? (
+									<>
+										<Sun className={style.icon} /> Light
+										Mode
+									</>
+								) : (
+									<>
+										<Moon className={style.icon} /> Dark
+										Mode
+									</>
+								)}
+							</>
+						)}
+					</div>
+				</nav>
+			</header>
+			<main>{children}</main>
 		</>
 	);
 }
