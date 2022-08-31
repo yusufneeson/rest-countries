3795ca0d5d6a2e8fe236d94c37a04ea111938ba4
yusufneeson <yusufneeson@gmail.com>
2022-08-31 10:23:50 +0700

    Added country details page

---
+++ src/pages/country/[id].jsx
@@ -0,0 +1,12 @@
+import Container from "../../components/Container/Container";
+import HomeLayout from "../../layout/HomeLayout";
+
+function Country() {
+	return (
+		<HomeLayout>
+			<Container />
+		</HomeLayout>
+	);
+}
+
+export default Country;
