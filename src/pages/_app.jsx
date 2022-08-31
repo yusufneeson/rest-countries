12122034bb91e9685ad09833364af8c9e9b85b6b
yusufneeson <yusufneeson@gmail.com>
2022-08-31 10:54:19 +0700

    _app: Apply nprogress

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,24 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import nProgress from "nprogress";
 
 function MyApp({ Component, pageProps }) {
+	const router = useRouter();
+
+	useEffect(() => {
+		router.events.on("routeChangeStart", () => nProgress.start());
+		router.events.on("routeChangeComplete", () => nProgress.done());
+		router.events.on("routeChangeError", () => nProgress.done());
+
+		return () => {
+			router.events.off("routeChangeStart", () => nProgress.start());
+			router.events.off("routeChangeComplete", () => nProgress.done());
+			router.events.off("routeChangeError", () => nProgress.done());
+		};
+	}, [router]);
+
 	return (
 		<ThemeProvider attribute="class" defaultTheme="light">
 			<Component {...pageProps} />
