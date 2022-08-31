cb764dbacfbd06fb53ed2ab55e2e3e239417781f
yusufneeson <yusufneeson@gmail.com>
2022-08-31 13:30:36 +0700

    Countries Component: Set filtered result

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,9 +1,24 @@
 import { useEffect, useState } from "react";
+import { useFilterContext } from "../../context/filter.context";
 import Card from "../Card/Card";
 import style from "./countries.module.css";
 
 function Countries({ countries }) {
+	const { state } = useFilterContext();
 	const [results, setResults] = useState(countries);
+
+	useEffect(() => {
+		const filteredCountry = countries.filter((c) => {
+			return (
+				c.name.toLowerCase().includes(state.country) &&
+				(state.region !== ""
+					? c.region.toLowerCase() === state.region
+					: true)
+			);
+		});
+
+		setResults(filteredCountry);
+	}, [state.country, state.region, countries]);
 
 	return (
 		<section className={style.countries}>
